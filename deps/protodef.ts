a177d4e63885c7aca672d3e75b1e1f06379c364a
Blocksnmore <51055767+Blocksnmore@users.noreply.github.com>
2021-11-23 17:43:13 -0600

    git weird

---
+++ deps/protodef.ts
@@ -0,0 +1,8 @@
+/**
+ * Minecraft-Protocol
+ *
+ * A port of node-minecraft-protocol to Deno
+ * ported by Blocks_n_more
+ */
+
+// TODO: Manually port this due to the fact esm.sh doesn't fully port it
